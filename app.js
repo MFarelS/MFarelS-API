9d22d073e035a323e1d4d6ca4ec8a215456cc92e
MFarelS <58540890+MFarelS@users.noreply.github.com>
2021-08-18 05:28:52 +0000

    Coded By MFarelS
    Zahra

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+var app = express();
+
+app.use(cors({ origin: '*' }));
+app.set('json spaces', 2);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'hasil')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
